af61c1c7c461fae23f6679f3cf33e2a42baf92a4
Henri Delozanne <hnr.delozanne@gmail.com>
2020-04-09 10:44:00 +0200

    Increase box-shadow on navbar elements.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -193,13 +193,13 @@ export default {
 
 .selects .el-select {
   margin: 5px 0;
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, .7);
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, .9);
 }
 
 .search-bar {
   border-radius: 5px;
   background-image: linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%);
-  box-shadow: 2px 2px 8px rgba(0, 0, 0, .7);
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, .9);
 }
 
 .menu-burger {
